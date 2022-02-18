988fb874069d6322af6ae3132aa5426767f923bf
Kamil Zak <kamilzak00@gmail.com>
2022-02-18 10:03:48 +0000

    finished dependency + concurrency

---
+++ concurrency/concurrency.go
@@ -0,0 +1,40 @@
+package concurrency
+
+import "net/http"
+
+type WebsiteChecker func(string) bool
+type result struct {
+	string
+	bool
+}
+
+func CheckWebsite(url string) bool {
+	response, err := http.Head(url)
+	if err != nil {
+		return false
+	}
+
+	if response.StatusCode != http.StatusOK {
+		return false
+	}
+
+	return true
+}
+
+func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
+	results := make(map[string]bool)
+	resultChannel := make(chan result)
+
+	for _, url := range urls {
+		go func(u string) {
+			resultChannel <- result{u, wc(u)}
+		}(url)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		r := <-resultChannel
+		results[r.string] = r.bool
+	}
+
+	return results
+}
